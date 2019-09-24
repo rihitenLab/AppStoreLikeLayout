817f5858c1ec1160f4128219d8819cfa386abe57
rihitenLab <rihitenlab@gmail.com>
2019-09-24 20:00:49 +0900

    Initial Commit

---
+++ AppStoreLikeLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreLikeLayout
+//
+//  Created by 松岡 利人 on 2019/09/24.
+//  Copyright © 2019 rihitenLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
